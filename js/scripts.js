a206728f945eacec35c7c2e5d13b5c62e4150036
Eric Liu <liueric@bu.edu>
2018-10-27 16:09:11 -0400

    Added back animation and set overflow to hidden on index.html Added in jquery script tag and animations on img-container classes.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".img-container").hover(function() {
+    $(this).children(".project-details").fadeToggle();
+    });
+});
+
+$(document).ready(function() {
+  $(".img-container-small").hover(function() {
+    $(this).children(".project-details").fadeToggle();
+    });
+});
